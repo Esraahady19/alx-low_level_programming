a55e0917fb01f18a7fb38f550d23cd4f40e6a2bb
Esraahady19 <Esraa@example.com>
2023-07-20 19:20:37 -0700

    a

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @x: The letter to be checked
+ * Return: 1 for upper letter or 0 for any else
+*/
+
+int _isupper(int x)
+
+{
+
+	if (x >=65 && <+90)
+	{
+		return (1);
+	}
+	return (0);
+}
