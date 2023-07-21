fe801bf8e39702af282cece9b1990644141583f0
Esraahady19 <Esraa@example.com>
2023-07-21 09:48:01 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,17 @@
 
 /**
  * print_numbers - Print the numbers from 0 to 9
- * @x: The input character
+ *
  * Return: Void
  */
 
 void print_numbers(void)
 {
-	int x;
+	char c;
 
-	for (x = '0'; x <= '9'; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(x);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
