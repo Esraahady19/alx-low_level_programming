95b154eadd9d0f1e2b6bce485b4e061df7705ea0
Esraahady19 <Esraa@example.com>
2023-07-20 20:06:57 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,19 +2,18 @@
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
- * @c: Number to check
- * Return: The numbers since 0 up to 9
+ * Return:void
  */
 
 void print_numbers(void)
 
 {
 
-	int c;
+	char c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c +'0');
+		_putchar('c');
 	}
 	_putchar('\n');
 }
