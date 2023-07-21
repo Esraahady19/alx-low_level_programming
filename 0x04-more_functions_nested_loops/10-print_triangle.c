165b90b1564b92a2504d97e292188b710cdd9b5b
Esraahady19 <Esraa@example.com>
2023-07-21 16:47:45 -0700

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,12 @@ void print_triangle(int size)
 	int i, j;
 
 	if (size > 0)
+	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				if (j > size - (i + 1))
+				if (j < size - (i + 1))
 				{
 					_putchar(' ');
 				}
@@ -26,6 +27,7 @@ void print_triangle(int size)
 			}
 			_putchar('\n');
 		}
+	}
 }
 
 
